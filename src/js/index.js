e2882ea25e9ac26cbdd05d9fd982c07e30857b71
batalygin <batalgin@reksoft.ru>
2018-09-08 13:27:03 +0300

    Sources updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,24 @@
-import './../sass/styles.scss';
+import 'babel-polyfill';
 import _ from 'lodash';
 
+import './../sass/styles.scss';
+
 const getHeader = () => {
-    const helloWebpack = _.join(_.values({
-        a: 'Hello',
-        b: 'webpack'
-    }), ' ');
+  const helloWebpack = _.join(['Hello', 'webpack!'], ' ');
+  console.log(helloWebpack);
+  const element = document.createElement('h1');
 
-    const element = document.createElement('h1');
+  element.innerHTML = helloWebpack;
 
-    element.innerHTML = helloWebpack;
-
-    return element;
+  return element;
 };
 
 document.body.appendChild(getHeader());
+
+const o = {
+  foo: {
+    bar: null
+  }
+};
+
+console.log(o?.foo?.bar?.baz ?? 'default');
